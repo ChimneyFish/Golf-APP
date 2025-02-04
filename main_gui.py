3e628192c0e95588c29fd252233f6247fc8225c4
ChimneyFish <hottyskipper@gmail.com>
2025-02-04 03:07:15 -0800

    commit

--- main_gui.py
+++ main_gui.py
@@ -31,7 +31,7 @@ class GolfRangeFinder(QWidget):
         self.setWindowTitle("Golf Range Finder & Scorekeeper")
         self.setGeometry(0, 0, 800, 480)
         
-        self.scores = [[0] * 9 for _ in range(4)]  # 4 players, 9 holes per page
+        self.scores = [[0] * 18 for _ in range(4)]  # 4 players, 18 holes
         self.current_page = 0
         self.players = ["Player 1", "Player 2", "Player 3", "Player 4"]
         self.pin_locations = {}
@@ -48,6 +48,11 @@ class GolfRangeFinder(QWidget):
         self.title_label.setStyleSheet("font-size: 18px; font-weight: bold; color: green;")
         self.layout.addWidget(self.title_label)
         
+        self.course_name_input = QLineEdit(self)
+        self.course_name_input.setPlaceholderText("Enter Course Name")
+        self.course_name_input.mousePressEvent = self.show_keyboard
+        self.layout.addWidget(self.course_name_input)
+        
         self.stack = QStackedWidget()
         self.pages = [self.create_score_page(i) for i in range(2)]
         for page in self.pages:
@@ -61,6 +66,10 @@ class GolfRangeFinder(QWidget):
         self.page_switch_btn = QPushButton("Switch to Back 9", self)
         self.page_switch_btn.clicked.connect(self.switch_page)
         self.layout.addWidget(self.page_switch_btn)
+        
+        self.save_course_btn = QPushButton("Save Course", self)
+        self.save_course_btn.clicked.connect(self.save_course_data)
+        self.layout.addWidget(self.save_course_btn)
         
         self.drive_distance_btn = QPushButton("Mark Tee Shot", self)
         self.drive_distance_btn.clicked.connect(self.mark_tee_shot)
@@ -80,14 +89,14 @@ class GolfRangeFinder(QWidget):
         self.score_inputs = [[None] * 9 for _ in range(4)]
         
         for i in range(4):
-            player_label = QLabel(self.players[i])
-            player_label.setStyleSheet("font-size: 14px; font-weight: bold;")
-            grid.addWidget(player_label, i, 0)
+            player_input = QLineEdit(self.players[i])
+            player_input.mousePressEvent = lambda event, idx=i: self.show_keyboard_for_player(idx)
+            grid.addWidget(player_input, i, 0)
             
             for j in range(9):
                 spinbox = QSpinBox()
                 spinbox.setRange(0, 10)
-                spinbox.setValue(self.scores[i][j])
+                spinbox.setValue(self.scores[i][j + (page_idx * 9)])
                 spinbox.valueChanged.connect(lambda value, row=i, col=j + (page_idx * 9): self.update_score(row, col, value))
                 self.score_inputs[i][j] = spinbox
                 grid.addWidget(spinbox, i, j + 1)
@@ -97,7 +106,7 @@ class GolfRangeFinder(QWidget):
         return widget
     
     def update_score(self, player, hole, value):
-        self.scores[player][hole % 9] = value
+        self.scores[player][hole] = value
         total = sum(sum(player_scores) for player_scores in self.scores)
         self.total_score_label.setText(f"Total Scores: {total}")
     
@@ -111,10 +120,12 @@ class GolfRangeFinder(QWidget):
             with open(data_file, "r") as f:
                 self.pin_locations = json.load(f)
     
-    def save_course_data(self, course_name):
-        self.pin_locations[course_name] = self.get_gps_location()
-        with open(data_file, "w") as f:
-            json.dump(self.pin_locations, f)
+    def save_course_data(self):
+        course_name = self.course_name_input.text()
+        if course_name:
+            self.pin_locations[course_name] = self.get_gps_location()
+            with open(data_file, "w") as f:
+                json.dump(self.pin_locations, f)
     
     def get_gps_location(self):
         try:
@@ -138,6 +149,17 @@ class GolfRangeFinder(QWidget):
                 print("Could not get current location")
         else:
             print("Tee shot location not set")
+    
+    def show_keyboard(self, event):
+        keyboard = OnScreenKeyboard(self)
+        if keyboard.exec():
+            self.course_name_input.setText(keyboard.get_text())
+    
+    def show_keyboard_for_player(self, player_idx):
+        keyboard = OnScreenKeyboard(self)
+        if keyboard.exec():
+            self.players[player_idx] = keyboard.get_text()
+            self.initUI()
     
 if __name__ == "__main__":
     app = QApplication(sys.argv)
