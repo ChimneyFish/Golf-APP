b2a78b9cd46355edcbeeae7f46a39c7f1309a577
ChimneyFish <hottyskipper@gmail.com>
2025-02-23 20:10:39 -0800

    l

--- .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/dists/ai_caddy/obj/local/arm64-v8a/objs-debug/webp/src/dsp/ssim.o.d
+++
@@ -1,8 +0,0 @@
-/home/jackmehoff/Golf-APP/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/arm64-v8a/objs-debug/webp/src/dsp/ssim.o: \
-  /home/jackmehoff/Golf-APP/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/dsp/ssim.c \
-  /home/jackmehoff/Golf-APP/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/dsp/dsp.h \
-  /home/jackmehoff/Golf-APP/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/types.h
-
-/home/jackmehoff/Golf-APP/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/dsp/dsp.h:
-
-/home/jackmehoff/Golf-APP/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/types.h:
